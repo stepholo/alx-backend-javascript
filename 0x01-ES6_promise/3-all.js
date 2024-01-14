e8e05a5ddbf81a0aa85c2c4f2509c1e3bbc196cd
stepholo <oloobrian89@gmail.com>
2024-01-14 09:28:27 +0000

    all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  // Use Promise.all to collectively resolve all promises
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      // Log the necessary information to the console
-      console.log(`Body: ${photoResponse.body}`);
-      console.log(`First Name: ${userResponse.firstName}`);
-      console.log(`Last Name: ${userResponse.lastName}`);
-    })
+  const p1 = uploadPhoto();
+  const p2 = createUser();
+
+  return Promise.all([p1, p2]).then((value) => {
+    console.log(`${value[0].body} ${value[1].firstName} $value[1].lastName}`);
+  })
     .catch(() => { console.log('Signup system offline'); });
 }
