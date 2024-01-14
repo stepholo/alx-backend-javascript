784ae6a38a1fd71e882f59e962104f533ed58016
stepholo <oloobrian89@gmail.com>
2024-01-14 09:01:10 +0000

    then

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch((error) => {
+      console.error('Error:', error.message);
+      return new Error(); // Return an empty Error object for rejections
+    });
+}
+
+module.export = handleResponseFromAPI;
