06220ed7cc960d2c88d6c0f9e6219c8fa95646f5
stepholo <oloobrian89@gmail.com>
2024-02-24 19:58:43 +0000

    ES6 - Classes

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,52 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = name;
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else {
+      this._length = length;
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('must be a array');
+    } else {
+      this._students = students;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw new TypeError('must be a array');
+    }
+    this._students = newStudents;
+  }
+}
---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency { /* eslint-disable */
+    constructor(code, name) {
+        if (typeof code !== 'string') {
+            throw new TypeError('Code must be a string')
+        }else {
+            this._code = code
+        }
+        if (typeof name !== 'string') {
+            throw new TypeError('Name must be a string')
+        }else {
+            this._name = name
+        }   
+    }
+
+    get code () {
+        return this._code
+    }
+
+    set code (newCode) {
+        if (typeof code !== 'string') {
+            throw new TypeError('Code must be a string')
+        }
+    }
+
+    get name () {
+        return this._name
+    }
+
+    set name (newName) {
+        if (typeof newName !== 'string') {
+            throw new TypeError('Name must be a string')
+        }
+    }
+
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`
+    };
+}
