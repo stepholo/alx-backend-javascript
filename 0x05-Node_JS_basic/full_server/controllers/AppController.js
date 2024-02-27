00922a1b441d972cea11cf704a7373d3c268d601
stepholo <oloobrian89@gmail.com>
2024-02-27 22:37:54 +0000

    full server

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+module.exports = AppController;
---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const express = require('express');
+const AppController = require('../controllers/AppController');
+const StudentsController = require('../controllers/StudentsController');
+
+const router = express.Router();
+
+router.get('/', AppController.getHomepage);
+
+router.get('/students', StudentsController.getAllStudents);
+
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
+
+module.exports = router;
